bd15ce0a1e9ae83a03dd7bdc7334f9da79664091
Milos9304 <prokop.milos@gmail.com>
2017-03-11 00:42:22 +0000

    MainActivity with error

--- app/src/main/java/com/ed/inf1/ardronehack17/MainActivity.java
+++ app/src/main/java/com/ed/inf1/ardronehack17/MainActivity.java
@@ -2,6 +2,9 @@ package com.ed.inf1.ardronehack17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.basic_control_button );
+
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               Intent intent = new Intent(getApplicationContext(), PathDraw.class);
+                startIntent(intent);
+            }
+        });
+
     }
+
+
+
+
 }
