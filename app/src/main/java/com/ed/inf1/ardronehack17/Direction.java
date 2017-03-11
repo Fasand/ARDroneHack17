2922cc44160fb49a1d6bd57a3e4d2ef232b01f49
Narrik <kristiankalinak@gmail.com>
2017-03-11 02:04:10 +0000

    first edit

--- app/src/main/java/com/ed/inf1/ardronehack17/Direction.java
+++ app/src/main/java/com/ed/inf1/ardronehack17/Direction.java
@@ -5,4 +5,20 @@ package com.ed.inf1.ardronehack17;
  */
 
 public class Direction {
+    private static int startx, starty, roll, pitch, yaw, x1, y1;
+    public static int[] Move (int x,int y, boolean photo){
+            x1 = Math.abs(startx-x);
+            y1 = Math.abs(starty-y);
+        if (x>startx)
+            roll = ((x1+y1)/x1)*100;
+        if (x<startx)
+            roll = ((x1+y1)/x1)*(-100);
+        if (y>starty)
+            pitch = ((x1+y1)/y1)+100;
+        if (y<starty)
+            pitch = ((x1+y1)/y1)*(-100);
+        int[] directions = new int[]{roll, pitch, yaw};
+        return directions;
+
+    }
 }
